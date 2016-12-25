908761fb49c147dd23f919c3646c4525d6dafd59
wmcalyj <mengchaowang@gmail.com>
2016-12-25 00:32:21 -0600

    assignment 1

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.checkItems = function () {
+      var items = $scope.items;
+      if(!items){
+        $scope.state = 'Please enter data first'
+      }else {
+        var itemArrays = items.split(",");
+        var count = 0;
+        for (var i = 0; i < itemArrays.length; i++) {
+          if(itemArrays[i]){
+            count++;
+          }
+        }
+        if(count <= 3){
+          $scope.state = 'Enjoy!';
+        }else {
+          $scope.state = 'Too much!';
+        }
+      }
+    }
+  }
+
+})();
