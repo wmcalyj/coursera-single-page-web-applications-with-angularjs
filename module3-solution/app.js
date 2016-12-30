26b1e64438a021b49243212ff0881d34d696ce29
wmcalyj <mengchaowang@gmail.com>
2016-12-30 14:39:21 -0600

    Assignment 3

---
+++ module3-solution/app.js
@@ -0,0 +1,71 @@
+(function () {
+  'use restrict';
+
+  angular.module('NarrowItDownApp',[])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .constant('BaseURL', "https://davids-restaurant.herokuapp.com/")
+  .constant('PathURI', "menu_items.json")
+  .directive('foundItems', FoundItemsDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        foundResults: '<',
+        onRemove: '&'
+      }
+    };
+
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var controller = this;
+  }
+
+  MenuSearchService.$inject = ['$http','BaseURL', 'PathURI'];
+  function MenuSearchService($http, BaseURL, PathURI) {
+    var service = this;
+    service.getMatchedMenuItems = function (searchTerm) {
+      var response =  $http({
+        method: "GET",
+        url: (BaseURL + PathURI)
+      });
+      return response.then(function (result){
+        var results = result.data;
+        var items = results.menu_items;
+        if(!searchTerm){
+          return items;
+        }
+        var found = [];
+        for (var i = 0; i < items.length; i++) {
+          if(items[i].description.indexOf(searchTerm) >= 0){
+            found.push(items[i]);
+          }
+        }
+        return found;
+      });
+    }
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var controller = this;
+    controller.narrowDown = function () {
+    var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
+    promise.then(function (found) {
+      controller.found = found;
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+
+    };
+
+    controller.onRemove = function (index) {
+      console.log("Remove index: ", index);
+      controller.found.splice(index, 1);
+    };
+  }
+})();
